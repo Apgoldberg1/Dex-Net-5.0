2a7e43830b998f20098487a658c3894b5e7c9fc9
apgoldberg1 <apgoldberg@gmail.com>
2024-04-25 18:37:41 +0000

    time_benchmark

--- scripts/time_benchmark.py
+++ scripts/time_benchmark.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def img_dim_scale_plot(fcgqcnn, fake_fcgqcnn):
-    loops = 10
-    batch = 100
+    loops = 4
+    batch = 128
 
     time_log1, time_log2 = [[],[],[]], []
 
     with torch.no_grad():
         # warm-up inference
         fcgqcnn(torch.zeros(1, 1, 40, 40).to("cuda"))
-        start_dim, end_dim, step = 35, 100, 5
+        start_dim, end_dim, step = 40, 90, 5
 
         print(f"fcgqcnn {loops} loops of batch size {batch} of varying image sizes")
         for i in range(3):
@@ -27,22 +27,28 @@ def img_dim_scale_plot(fcgqcnn, fake_fcgqcnn):
         time_log1 = np.array(time_log1)
         time_log1 = np.mean(time_log1, axis=0)
 
-    # plt.plot(list(range(start_dim, end_dim, step)), time_log1, label = "FC-GQ-CNN")
-    # plt.title("FC-GQ-CNN Scaling")
-    plt.plot(list(range(start_dim, end_dim, step)), time_log2, label = "Naive Approach", color = "orange")
-    plt.title("Naive Approach Scaling")
+        fcgqcnn(torch.zeros(1, 1, 40, 40).to("cuda"))
+        start_dim2, end_dim2, step2 = 40, 90, 15
+
+        for img_dim in range(start_dim2, end_dim2, step2):
+            start = time.time()
+            for _ in range(loops):
+                data = torch.rand((batch, 1, img_dim, img_dim)).to(device)
+                fake_fcgqcnn(data)
+            print("time:", time.time() - start)
+            time_log2.append(time.time() - start)
+
+    plt.plot(list(range(start_dim, end_dim, step)), time_log1, label = "FC-GQ-CNN")
+    plt.title("FC-GQ-CNN vs. Naive Approach Scaling")
+    plt.plot(list(range(start_dim2, end_dim2, step2)), time_log2, label = "Naive Approach", color = "orange")
+    plt.yscale('log')
     plt.xlabel("image dimension (px)")
-    plt.ylabel("inference time per 1000 images (sec)")
+    plt.ylabel("inference time per 512 images (log scale, seconds)")
     plt.xticks(list(range(start_dim, end_dim, step)))
     plt.legend()
-    # plt.savefig("outputs/fcgqcnn_time_scale.jpg")
-    plt.savefig("outputs/naive_fcgqcnn_time_scale.jpg")
+    plt.savefig("outputs/fcgqcnn_time_scale.jpg")
     plt.show()
     plt.close()
-
-    # plt.plot(img_size2, time_log2, label = "Naive Approach")
-
-    
 
 def inference_benchmark(gqcnn_path, fcgqcnn_path, efficientnet_path, device):
     gqcnn_w = torch.load(gqcnn_path)
@@ -64,8 +70,8 @@ def inference_benchmark(gqcnn_path, fcgqcnn_path, efficientnet_path, device):
     efficientnet_gqcnn.eval()
 
     loops = 0
-    batch = 128
-    img_dim = 70
+    batch = 64
+    img_dim = 90
 
     img_dim_scale_plot(high_res_fcgqcnn, fake_fcgqcnn)
 
@@ -98,6 +104,7 @@ def inference_benchmark(gqcnn_path, fcgqcnn_path, efficientnet_path, device):
         print("efficientnet_gqcnn")
         print("time:", time.time() - start)
 
+        batch = 8192
         start = time.time()
         for i in range(loops):
             data = torch.rand((batch, 1, 32, 32)).to(device)
