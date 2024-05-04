08bb5c75929fa3e28ab8186d81dc74d7715dd0c4
Ryan Hoque <ryanhoque@berkeley.edu>
2024-05-04 05:26:40 +0000

    testing

--- scripts/fcgqcnn_directory.py
+++ scripts/fcgqcnn_directory.py
@@ -5,12 +5,16 @@ import torch
 import numpy as np
 import math
 from dexnet.grasp_model import DexNet3FCGQCNN as FCGQCNN
+from dexnet.grasp_model import DexNet3, fakeSuctionFCGQCNN
 import matplotlib.pyplot as plt
 import cv2
 import os
 
+fake_fc = False # set to True for for-loop, False for FCGQCNN beta
+
 # change directory as desired (should probably be a CLI arg)
-directory = '/home/ryanhoque/scene_data/CokePlasticSmallGrasp_800_tex'
+#directory = '/home/ryanhoque/scene_data2/CokePlasticSmallGrasp_800_tex'
+directory = '/home/ryanhoque/scene_data/more_objects/book'
 if not os.path.exists(directory+'/grasp'):
     os.makedirs(directory+'/grasp')
     os.makedirs(directory+'/grasp_viz')
@@ -21,6 +25,13 @@ fcgqcnn = FCGQCNN()
 fcgqcnn.load_state_dict(torch.load(model_weights_path))
 fcgqcnn.eval()
 fcgqcnn.to("cuda")
+
+dexnet3_weights_path = "model_zoo/just_image.pth"
+gqcnn = DexNet3()
+gqcnn.load_state_dict(torch.load(dexnet3_weights_path))
+fake_model = fakeSuctionFCGQCNN(gqcnn)
+fake_model.eval()
+fake_model.to('cuda')
 
 def process_img(x):
     x = 255 - x # blender depth is inverted
@@ -37,11 +48,16 @@ def process_img(x):
     return x
 
 for i in range(100):
+    #img = cv2.imread("{}/depth/img_{}.png".format(directory, i)).mean(axis=2)
     img = cv2.imread("{}/depth/{}.png".format(directory, str(i).zfill(4))).mean(axis=2)
     img = process_img(img)
     output_path = "{}/grasp/{}.npy".format(directory, i)
     with torch.no_grad():
-        output = fcgqcnn(img).to('cpu').numpy().squeeze()
+        if fake_fc:
+            output = fake_model(img)
+        else:
+            output = fcgqcnn(img)
+        output = output.to('cpu').numpy().squeeze()
         print(output.min(), output.max())
         # threshold value
         output[output < 0.2] = 0.
